b929ebcee5672afadac8c29ccd5f0729296bee05
43a1723 <hai1723notgay@gmail.com>
2024-07-17 09:30:44 +0700

    Update zeroobf.py

--- zeroobf.py
+++ zeroobf.py
@@ -2,14 +2,17 @@ import random
 import base64
 import argparse
 import zlib
+import marshal
 from python_minifier import minify as pyminify
 
 class ZeroObfuscator:
     def __init__(self):
+        self._valid_identifiers = [chr(x) for x in range(1000) if self.set_variable_from_char(chr(x))]
         self.zeroobf = self.generate_var(100)
         self.obfcode = f"""
 import base64
 import zlib
+import marshal
 # https://github.com/werearecat/zeroobf
 # obf code
 {self.zeroobf}var = ""
@@ -18,8 +21,29 @@ import zlib
 """
         print("ZeroObfuscator initialized.")
 
+    def set_variable_from_char(self, char):
+        """
+        Thực hiện exec để gán giá trị cho biến có tên là ký tự đầu vào.
+        Nếu exec gây lỗi, hàm trả về False.
+        """
+        if len(char) != 1:
+            raise ValueError("Input must be a single character")
+        
+        try:
+            # Tạo lệnh exec để gán giá trị cho biến
+            exec(f"{char} = '{char}'")
+            # Trả về True nếu exec thành công
+            return True
+        except Exception as e:
+            # Xử lý lỗi và trả về False nếu có lỗi xảy ra
+            return False
+
     def generate_var(self, length=10):
-        return ''.join(f'隐藏_{random.randint(0, 255):02x}' for _ in range(length))
+        valid = random.choice(self._valid_identifiers)
+        valid2 = random.choice(self._valid_identifiers)
+        valid = valid * 10
+        valid2 = valid2 * 10
+        return ''.join(f'{valid}{valid2}_{random.randint(0, 255):02x}' for _ in range(length))
 
     def string_to_hex(self, s):
         return ''.join(f'\\x{ord(c):02x}' for c in s)
@@ -42,14 +66,15 @@ import zlib
 {self.zeroobf}var += base64.b64decode("{encoded_line}").decode() + "\\n"
 {self.zeroobf}var2 += f"{self.generate_random_zeroes(20)}"
 """
-            compressed_code = zlib.compress(encoded_lines_haha.encode()).hex()
-            encoded_lines += f"""\nexec(zlib.decompress(bytes.fromhex("{compressed_code}")).decode())"""
+            marshal_code = marshal.dumps(compile(encoded_lines_haha, '<string>', 'exec'))
+            compressed_code = zlib.compress(marshal_code).hex()
+            encoded_lines += f"""\nexec(marshal.loads(zlib.decompress(bytes.fromhex("{compressed_code}"))))"""
             print(f"Processed line {i}/{total_lines} Now {len(encoded_lines)} bytes")
 
         final_code = self.obfcode + encoded_lines
         final_code += f"\n\nexec({self.zeroobf}var)"
-        print("minify code")
+        print("Minifying code...")
         minified_code = pyminify(final_code)
-        print(f"minify {len(final_code)} bytes => {len(minified_code)} bytes")
+        print(f"Minified {len(final_code)} bytes => {len(minified_code)} bytes")
         print("Code obfuscation complete.")
         return minified_code
