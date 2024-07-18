c139f9ba7850b631ee55cccf16df4c3c1dd8a9ba
43a1723 <hai1723notgay@gmail.com>
2024-07-18 15:55:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import zlib
 class ZeroObfuscator:
     def __init__(self):
         self._valid_identifiers = [chr(x) for x in range(1000) if self.set_variable_from_char(chr(x))]
-        self.zeroobf = self.generate_var(100)
+        self.zeroobf = self.generate_var()
         self.obfcode = f"""
 import base64
 import zlib
@@ -16,9 +16,9 @@ import zlib
 {self.zeroobf}var1 = ""
 {self.zeroobf}var2 = ""
 {self.zeroobf}var3 = 0
+{self.zeroobf}隐藏 = exec
 """
         self.zeroexec = f"{self.zeroobf}隐藏"
-        self.zeroif = f"{self.zeroobf}隐藏_"
         print("ZeroObfuscator initialized.")
 
     def set_variable_from_char(self, char):
@@ -38,16 +38,15 @@ import zlib
             # Xử lý lỗi và trả về False nếu có lỗi xảy ra
             return False
 
-    def generate_var(self, length=10):
+    def generate_var(self):
         length = random.randint(100, 250)
-        random_string = '\t' * length
-        return random_string
+        return '\t' * length
 
     def string_to_hex(self, s):
         return ''.join(f'\\x{ord(c):02x}' for c in s)
 
     def string_to_hex_fake(self, s):
-        return ''.join(f'\t\t\t\t\t\t' for c in s)
+        return ''.join(f'\t\t\t\t\t\t' for _ in s)
 
     def generate_random_zeroes(self, length):
         return '\\x00' * length
@@ -64,19 +63,17 @@ import zlib
 {self.zeroobf}var += base64.b64decode("{encoded_line}").decode() + "\\n"
 {self.zeroobf}var2 += f"{self.generate_random_zeroes(20)}"
 {self.zeroobf}var3 += 1
-{self.zeroif} {self.zeroobf}var3 == {total_lines}:
+if {self.zeroobf}var3 == {total_lines}:
     {self.zeroexec}({self.zeroobf}var)
     {self.zeroobf}var = ""
 """
-            compressed_code = zlib.compress(encoded_lines_haha.encode()).hex()
             encoded_lines += encoded_lines_haha
             print(f"Processed line {i}/{total_lines} Now {len(encoded_lines)} bytes")
 
         final_code_old = self.obfcode + encoded_lines
         final_code = self.obfcode + f"""\nexec(zlib.decompress(bytes.fromhex("{zlib.compress(encoded_lines.encode()).hex()}")).decode())"""
         
-        return final_code_old.replace("var1", f"\t\t\t\t").replace("var2", f"\t\t\t\t\t").replace("var3", f"\t\t\t\t\t\t").replace("var", f"\t\t\t")
-        # return final_code.replace("var1", f"\t\t\t\t").replace("var2", f"\t\t\t\t\t").replace("var3", f"\t\t\t\t\t\t").replace("var", f"\t\t\t")
+        return final_code_old
 
 def main():
     parser = argparse.ArgumentParser(description='Zero Obfuscator')
