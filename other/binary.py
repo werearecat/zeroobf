35c3aa6f703335e909401469ca8713082f66321a
43a1723 <hai1723notgay@gmail.com>
2024-07-21 18:08:07 +0700

    Update binary.py

--- other/binary.py
+++ other/binary.py
@@ -9,17 +9,17 @@ def obfcode(input_file, output_file):
     binary_code = ' '.join(format(ord(c), '08b') for c in code)
 
     length = 1
-    0 = "".join(random.choices(["\xa0", chr(8239)] + [chr(x) for x in range(8192, 8208)], k= length))
-    1 = "".join(random.choices(["\xa0", chr(8239)] + [chr(x) for x in range(8192, 8208)], k= length))
+    a0 = "".join(random.choices(["\xa0", chr(8239)] + [chr(x) for x in range(8192, 8208)], k= length))
+    a1 = "".join(random.choices(["\xa0", chr(8239)] + [chr(x) for x in range(8192, 8208)], k= length))
     space = "".join(random.choices(["\xa0", chr(8239)] + [chr(x) for x in range(8192, 8208)], k= length))
     fake = "".join(random.choices(["\xa0", chr(8239)] + [chr(x) for x in range(8192, 8208)], k= 1000))
-    binary_code_hidden = binary_code.replace("0", f"{0}").replace("1", f"{1}").replace(" ", f"{space}")
+    binary_code_hidden = binary_code.replace("0", f"{a0}").replace("1", f"{a1}").replace(" ", f"{space}")
 
     # Tạo mã nguồn để giải mã và thực thi
     obfuscated_code = f"""
 # https://github.com/werearecat/zeroobf
 # made with chatgpt :)
-exec("".join(chr(int(b, 2)) for b in "{binary_code_hidden}".replace("{0}", "0").replace("{1}", "1").replace("{space}", " ").replace("{fake}", "{fake}").split()))
+exec("".join(chr(int(b, 2)) for b in "{binary_code_hidden}".replace("{a0}", "0").replace("{a1}", "1").replace("{space}", " ").replace("{fake}", "{fake}").split()))
     """
     
     # Ghi file với mã hóa UTF-8
