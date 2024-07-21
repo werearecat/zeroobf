b3be4dea7f1c08fedadd0a36df4090904ae633b4
43a1723 <hai1723notgay@gmail.com>
2024-07-21 17:52:36 +0700

    :) add trash

--- other/binary.py
+++ other/binary.py
@@ -12,13 +12,14 @@ def obfcode(input_file, output_file):
     0 = "".join(random.choices(["\xa0", chr(8239)] + [chr(x) for x in range(8192, 8208)], k= length))
     1 = "".join(random.choices(["\xa0", chr(8239)] + [chr(x) for x in range(8192, 8208)], k= length))
     space = "".join(random.choices(["\xa0", chr(8239)] + [chr(x) for x in range(8192, 8208)], k= length))
+    trash = "".join(random.choices(["\xa0", chr(8239)] + [chr(x) for x in range(8192, 8208)], k= 1000))
     binary_code_hidden = binary_code.replace("0", f"{0}").replace("1", f"{1}").replace(" ", f"{space}")
 
     # Tạo mã nguồn để giải mã và thực thi
     obfuscated_code = f"""
 # https://github.com/werearecat/zeroobf
 # made with chatgpt :)
-exec("".join(chr(int(b, 2)) for b in "{binary_code_hidden}".replace("{0}", "0").replace("{1}", "1").replace("{space}", " ").split()))
+exec("".join(chr(int(b, 2)) for b in "{trash}{binary_code_hidden}{trash}".replace("{0}", "0").replace("{1}", "1").replace("{space}", " ").replace("{trash}", "").split()))
     """
     
     # Ghi file với mã hóa UTF-8
