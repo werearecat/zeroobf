1939079725b3df0462e15c4cc48a300c4e0bf587
43a1723 <hai1723notgay@gmail.com>
2024-07-23 14:22:24 +0700

    Update binary.py

--- other/binary.py
+++ other/binary.py
@@ -7,13 +7,13 @@ def obfcode(input_file, output_file):
     binary_code = ' '.join(format(ord(c), '08b') for c in code)
     
     # Thay thế 0 và 1 bằng ký tự Unicode
-    binary_code_hidden = binary_code.replace("0", "").replace("1", "").replace("1", "\u200C").replace(" ", "")
+    binary_code_hidden = binary_code.replace("0", "").replace("1", "").replace(" ", "\\x00")
 
     # Tạo mã nguồn để giải mã và thực thi
     obfuscated_code = f"""
 # https://github.com/werearecat/zeroobf
 # made with chatgpt :)
-exec("".join(chr(int(b, 2)) for b in "{binary_code_hidden}".replace("", "0").replace("", "1").replace("", " ").split()))
+exec("".join(chr(int(b, 2)) for b in "{binary_code_hidden}".replace("", "0").replace("", "1").replace("\\x00", " ").split()))
     """
     
     # Ghi file với mã hóa UTF-8
