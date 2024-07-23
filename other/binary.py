07016a56da5dd86bbe04eda42842ae8208ad52c8
43a1723 <hai1723notgay@gmail.com>
2024-07-23 13:58:27 +0700

    old version I think no need more

--- other/binary.py
+++ other/binary.py
@@ -1,8 +1,3 @@
-import random
-
-def string_to_hex(s):
-    return ''.join(f'\\x{ord(c):02x}' for c in s)
-
 def obfcode(input_file, output_file):
     # Đọc file với mã hóa UTF-8
     with open(input_file, 'r', encoding='utf-8') as f:
@@ -10,29 +5,19 @@ def obfcode(input_file, output_file):
 
     # Chuyển mỗi ký tự thành mã nhị phân 8 bit
     binary_code = ' '.join(format(ord(c), '08b') for c in code)
-
-    length = 1
     
-    a0 = "\u005F"
-    a1 = "_"
-    space = "\u200D"
-    fake = "\u005F_\u005F__\u005F___\u005F_"
-    binary_code_hidden = binary_code[::-1].replace("0", f"{a0}").replace("1", f"{a1}").replace(" ", f"{space}")
+    # Thay thế 0 và 1 bằng ký tự Unicode
+    binary_code_hidden = binary_code.replace("0", "\u005F").replace("1", "_").replace("1", "\u200C").replace(" ", "\u200D")
 
     # Tạo mã nguồn để giải mã và thực thi
-    exec_ = "\u0674\u0674e\u0674\u0674x\u0674\u0674e\u0674\u0674c"
     obfuscated_code = f"""
-'''
 # https://github.com/werearecat/zeroobf
 # made with chatgpt :)
-{chr(10) * 1000}
-'''
-
-{exec_} = getattr(__import__('{string_to_hex("builtins")}'), '{string_to_hex("exec")}'); {exec_}g = lambda s: ''.join(chr(((ord(c) + (len(str({exec_}) * 15) - 360)) % 256)) for c in s) ;{exec_}({exec_}g("".join(chr(int(b, 2)) for b in "{binary_code_hidden}"[::-1].replace("{a0}", "0").replace("{a1}", "1").replace("{space}", " ").replace("{fake}", "{fake}").split())))
+exec("".join(chr(int(b, 2)) for b in "{binary_code_hidden}".replace("\u005F", "0").replace("_", "1").replace("\u200D", " ").split()))
     """
     
     # Ghi file với mã hóa UTF-8
     with open(output_file, 'w', encoding='utf-8') as f:
         f.write(obfuscated_code)
 
-obfcode('code.py', 'code_obf.py')
+obfcode('code.txt', 'code_obf.txt')
