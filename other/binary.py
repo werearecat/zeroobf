82429fbe67a1b51d624ff2df4924e7aae6aa0663
43a1723 <hai1723notgay@gmail.com>
2024-07-25 15:30:14 +0700

    Update binary.py

--- other/binary.py
+++ other/binary.py
@@ -2,8 +2,8 @@ def string_to_binary(s):
     return ''.join(format(ord(char), '08b') for char in s)
 
 def obf_binary(code):
-    decode_def = "def decode_bin(b):b=b.replace('\\rzeroobf\\r','1').replace('\\nzeroobf\\n','0');A=int(b,2);return A.to_bytes((A.bit_length()+7)//8,'big').decode()"
-    binary_destory = string_to_binary(code).replace('0', '\\nzeroobf\\n').replace('1', '\\rzeroobf\\r')
+    decode_def = "def decode_bin(b):b=b.replace('ŏ','1').replace('ŏ̆','0');A=int(b,2);return A.to_bytes((A.bit_length()+7)//8,'big').decode()"
+    binary_destory = string_to_binary(code).replace('0', 'ŏ̆').replace('1', 'ŏ')
     code = f"""
 {decode_def}
 exec(decode_bin("{binary_destory}"))
