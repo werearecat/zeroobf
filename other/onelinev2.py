f4ac3f9ea7fa6479cd24fc3f24c3a627d97cd169
43a1723 <hai1723notgay@gmail.com>
2024-07-28 09:33:46 +0700

    Update onelinev2.py

--- other/onelinev2.py
+++ other/onelinev2.py
@@ -1,45 +1,11 @@
 import argparse
-import marshal
-
-def string_to_hex(string):
-    return ''.join(f'\\x{ord(c):02x}' for c in string)
-
-def split_string(text):
-    # Tính toán điểm chia chuỗi
-    mid_index = len(text) // 2
-    # Tách chuỗi thành hai phần
-    first_part = text[:mid_index]
-    second_part = text[mid_index:]
-    return first_part, second_part
-
-def split_string1(text):
-    first_part, second_part = split_string(text)
-    return first_part
-
-def split_string2(text):
-    first_part, second_part = split_string(text)
-    return second_part
 
 def obfcode(s):
-    s = f"""
-# you are good
-exec("{string_to_hex(split_string1(s))}" + "{string_to_hex(split_string2(s))}")
-"""
-    s = f"""
-# you are good
-exec("{string_to_hex(split_string1(s))}" + "{string_to_hex(split_string2(s))}")
-"""
-    s = f"""
-# you are good
-exec("{string_to_hex(split_string1(s))}" + "{string_to_hex(split_string2(s))}")
-"""
-    XD_Anti = chr(len(str(exec)))
-    XD_Anti2 = "chr(len(str(\u0674\u0674\u0674)))"
-    XD = ''.join(f"""+chr(ord({XD_Anti2}) + {int(ord(c) - ord(XD_Anti))})""" for c in s)
+    XD = ''.join(f"""+chr(ord('A') + {int(ord(c) - ord('A'))})""" for c in s)
     code = f"""
 # https://github.com/werearecat/zeroobf
 # no name :)
-\u0674\u0674\u0674=exec;_NO(''{XD})
+exec(''{XD})
 """
     return code
 
