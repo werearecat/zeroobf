75828922efd3b8874e928739bb123fee8f2b6597
43a1723 <hai1723notgay@gmail.com>
2024-07-28 09:34:21 +0700

    Update onelinev2.py

--- other/onelinev2.py
+++ other/onelinev2.py
@@ -1,7 +1,7 @@
 import argparse
 
 def obfcode(s):
-    XD = ''.join(f"""+chr(ord('A') + {int(ord(c) - ord('A'))})""" for c in s)
+    XD = ''.join(f"""+chr(ord('\\n') + {int(ord(c) - ord('\n'))})""" for c in s)
     code = f"""
 # https://github.com/werearecat/zeroobf
 # no name :)
