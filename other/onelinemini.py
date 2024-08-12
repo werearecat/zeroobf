8dcdddb8f6d105c3087348d059ccc15649fad8dc
43a1723 <hai1723notgay@gmail.com>
2024-08-12 19:42:28 +0700

    Update onelinemini.py

--- other/onelinemini.py
+++ other/onelinemini.py
@@ -1,92 +1,25 @@
-import argparse
-import random
+import base64
+import marshal
+import bz2
+def encryptcode(codee):
+    compiled_code = compile(codee, '<string>', 'exec')
+    compressed_code = bz2.compress(marshal.dumps(compiled_code))
+    compressed_code_str = repr(compressed_code)
+    return f"exec(__import__('marshal').loads(__import__('bz2').decompress({compressed_code_str})))"
 
-def RandomChinaWord():
-    val = random.randint(0x4e00, 0x9fbf) 
-    return chr(val)
-
-def RandomChina(size: int):
-    words = ""
-    for i in range(size):
-        words += RandomChinaWord()
-    return words
-
-def generate_long_expression(target, depth=15):
-    expression = str(target)
-    for _ in range(depth):
-        operation = random.choice(['+', '-', '*', '/'])
-        next_number = random.randint(1, 10)
-        if operation == '/':
-            next_number = max(1, random.randint(1, 10))  # Ensure non-zero divisor
-        expression = f"({expression} {operation} {next_number})"
-        if operation == '+':
-            target -= next_number
-        elif operation == '-':
-            target += next_number
-        elif operation == '*':
-            target /= next_number
-        elif operation == '/':
-            target *= next_number
-    return expression
-
-def create_expression_with_target(target, depth=25):
-    while True:
-        expression = generate_long_expression(target, depth)
-        try:
-            if eval(expression) == target:
-                return expression
-        except ZeroDivisionError:
-            continue
-        except Exception as e:
-            print(f"Error creating expression: {e}")
-
-def encode(text):
-    # Bước 1: Đảo ngược chuỗi
-    reversed_text = text[::-1]
-    
-    # Bước 2: Chuyển đổi từng ký tự thành mã ASCII và cộng thêm một giá trị cố định (ví dụ 3)
-    encoded_chars = [(ord(char) + 3) for char in reversed_text]
-    
-    # Bước 3: Chuyển đổi lại mã ASCII thành ký tự
-    encoded_text = ''.join([chr(num) for num in encoded_chars])
-    
-    return encoded_text
-
-
-def obfcode(s):
-    
-    text = ''.join([chr(i) for i in range(0x4e00, 0x9fbf + 1)])
-    shuffled_text = ''.join(random.sample(text, len(text)))
-    newline = "\n"
-    XD = ''.join(f"""+WANNACRY({create_expression_with_target(ord(encode(c)))})""" for c in s)
-    code = f"""
-# https://github.com/werearecat/zeroobf
-# no name :)
-def WANNACRY(encoded_int):A=chr(encoded_int);B=[ord(A)-3 for A in A];C=''.join([chr(A)for A in B]);D=C[::-1];return D
-exec(''{XD})
-"""
-    code = code.replace("WANNACRY(13)", "'\\n'")
-    code = code.replace("WANNACRY", RandomChina(4))
-    code = code.replace("encoded_int", RandomChina(5))
-    
-    return code
-
-def main():
-    parser = argparse.ArgumentParser(description='1line Obfuscator')
-    parser.add_argument('-i', '--input', required=True, help='Input file')
-    parser.add_argument('-o', '--output', required=True, help='Output file')
-    
-    args = parser.parse_args()
-    
-    print(f"Reading input file: {args.input}")
-    with open(args.input, 'r', encoding='utf-8') as file:
-        code = file.read()
-    
-    obfuscated_code = obfcode(code)
-    
-    print(f"Writing obfuscated code to: {args.output}")
-    with open(args.output, 'w', encoding='utf-8') as file:
-        file.write(obfuscated_code)
-
-if __name__ == '__main__':
-    main()
+def obfuscate(content):
+    b64_content = base64.b64encode(content.encode()).decode()
+    index = 0
+    VARIABLE_NAME = "shit_" * 1000
+    code = f'{VARIABLE_NAME} = ""\n'
+    for _ in range(int(len(b64_content) / OFFSET) + 1):
+        _str = ''
+        for char in b64_content[index:index + OFFSET]:
+            byte = str(hex(ord(char)))[2:]
+            if len(byte) < 2:
+                byte = '0' + byte
+            _str += '\\x' + str(byte)
+        code += f'{VARIABLE_NAME} += "{_str}"\n'
+        index += OFFSET
+    code += f'exec(__import__("\\x62\\x61\\x73\\x65\\x36\\x34").b64decode({VARIABLE_NAME}.encode("\\x75\\x74\\x66\\x2d\\x38")).decode("\\x75\\x74\\x66\\x2d\\x38"))'
+    return encryptcode(code)
