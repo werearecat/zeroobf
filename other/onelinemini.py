8a7feab54e202e96b3118ee68b982722ac73e1a0
43a1723 <hai1723notgay@gmail.com>
2024-08-12 19:43:29 +0700

    Update onelinemini.py

--- other/onelinemini.py
+++ other/onelinemini.py
@@ -10,6 +10,7 @@ def encryptcode(codee):
 def obfuscate(content):
     b64_content = base64.b64encode(content.encode()).decode()
     index = 0
+    OFFSET = 10
     VARIABLE_NAME = "shit_" * 1000
     code = f'{VARIABLE_NAME} = ""\n'
     for _ in range(int(len(b64_content) / OFFSET) + 1):
@@ -23,3 +24,23 @@ def obfuscate(content):
         index += OFFSET
     code += f'exec(__import__("\\x62\\x61\\x73\\x65\\x36\\x34").b64decode({VARIABLE_NAME}.encode("\\x75\\x74\\x66\\x2d\\x38")).decode("\\x75\\x74\\x66\\x2d\\x38"))'
     return encryptcode(code)
+
+def main():
+    parser = argparse.ArgumentParser(description='1line Obfuscator')
+    parser.add_argument('-i', '--input', required=True, help='Input file')
+    parser.add_argument('-o', '--output', required=True, help='Output file')
+    
+    args = parser.parse_args()
+    
+    print(f"Reading input file: {args.input}")
+    with open(args.input, 'r', encoding='utf-8') as file:
+        code = file.read()
+    
+    obfuscated_code = obfcode(code)
+    
+    print(f"Writing obfuscated code to: {args.output}")
+    with open(args.output, 'w', encoding='utf-8') as file:
+        file.write(obfuscated_code)
+
+if __name__ == '__main__':
+    main()
