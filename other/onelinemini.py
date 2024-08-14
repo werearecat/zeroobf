e93d9f0c7eb1d70b62617a78014f16df55d1eb78
43a1723 <hai1723notgay@gmail.com>
2024-08-14 10:05:51 +0700

    Update onelinemini.py

--- other/onelinemini.py
+++ other/onelinemini.py
@@ -1,10 +1,7 @@
+import base64
 import marshal
 import bz2
 import argparse
-
-def xor_encrypt_decrypt(data, key):
-    """Encrypt or decrypt data using XOR with the given key."""
-    return bytes([b ^ key for b in data])
 
 def encryptcode(codee):
     compiled_code = compile(codee, '<string>', 'exec')
@@ -12,21 +9,22 @@ def encryptcode(codee):
     compressed_code_str = repr(compressed_code)
     return f"exec(__import__('marshal').loads(__import__('bz2').decompress({compressed_code_str})))"
 
-def obfuscate(content, key=0x42):  # Default XOR key is 0x42
-    # XOR encrypt the content
-    xor_content = xor_encrypt_decrypt(content.encode(), key)
-    hex_content = ''.join(f'\\x{byte:02x}' for byte in xor_content)
-    
+def obfuscate(content):
+    b64_content = base64.b64encode(content.encode()).decode()
     index = 0
     OFFSET = 10
     VARIABLE_NAME = "___" * 1000
     code = f'{VARIABLE_NAME} = ""\n'
-    for _ in range(int(len(hex_content) / OFFSET) + 1):
-        _str = hex_content[index:index + OFFSET]
+    for _ in range(int(len(b64_content) / OFFSET) + 1):
+        _str = ''
+        for char in b64_content[index:index + OFFSET]:
+            byte = str(hex(ord(char)))[2:]
+            if len(byte) < 2:
+                byte = '0' + byte
+            _str += '\\x' + str(byte)
         code += f'{VARIABLE_NAME} += "{_str}"\n'
         index += OFFSET
-    
-    code += f'exec(__import__("builtins").bytes.fromhex({VARIABLE_NAME}).decode().encode("latin1"))'
+    code += f'exec(__import__("\\x62\\x61\\x73\\x65\\x36\\x34").b64decode({VARIABLE_NAME}.encode("\\x75\\x74\\x66\\x2d\\x38")).decode("\\x75\\x74\\x66\\x2d\\x38"))'
     return encryptcode(code)
 
 def main():
@@ -34,15 +32,16 @@ def main():
     parser.add_argument('-i', '--input', required=True, help='Input file')
     parser.add_argument('-o', '--output', required=True, help='Output file')
     parser.add_argument('-l', '--layer', type=int, default=1, help='Obfuscation layer (default: 1)')
-    parser.add_argument('-k', '--key', type=int, default=0x42, help='XOR key (default: 0x42)')
 
     args = parser.parse_args()
 
+
+        
     print(f"Reading input file: {args.input}")
     with open(args.input, 'r', encoding='utf-8') as file:
         code = file.read()
     for _ in range(args.layer):
-        code = obfuscate(code, key=args.key)
+        code = obfuscate(code)
         print(f"{_}\{args.layer} obfuscated\n{len(code)} bytes!")
     
     print(f"Writing obfuscated code to: {args.output}")
@@ -51,3 +50,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+Thay vì sài base64 thì hãy sài xor hoặc cái khác
