143fe1e24f0a0463750226986a920a02ff906b87
43a1723 <hai1723notgay@gmail.com>
2024-08-14 09:24:34 +0700

    Update onelinemini.py

--- other/onelinemini.py
+++ other/onelinemini.py
@@ -6,26 +6,16 @@ import argparse
 def encryptcode(codee):
     compiled_code = compile(codee, '<string>', 'exec')
     compressed_code = bz2.compress(marshal.dumps(compiled_code))
-    compressed_code_str = "[" + ",".join(map(repr(compressed_code), byte_values)) + "]"
-    return f"exec(__import__('marshal').loads(__import__('bz2').decompress(bytes({compressed_code_str}))))"
+    compressed_code_str = repr(compressed_code)
+    return f"exec(__import__('marshal').loads(__import__('bz2').decompress({compressed_code_str}))"
 
 def obfuscate(content):
-    b64_content = base64.b64encode(content.encode()).decode()
-    index = 0
-    OFFSET = 10
-    VARIABLE_NAME = "___" * 1000
-    code = f'{VARIABLE_NAME} = ""\n'
-    for _ in range(int(len(b64_content) / OFFSET) + 1):
-        _str = ''
-        for char in b64_content[index:index + OFFSET]:
-            byte = str(hex(ord(char)))[2:]
-            if len(byte) < 2:
-                byte = '0' + byte
-            _str += '\\x' + str(byte)
-        code += f'{VARIABLE_NAME} += "{_str}"\n'
-        index += OFFSET
-    code += f'exec(__import__("\\x62\\x61\\x73\\x65\\x36\\x34").b64decode({VARIABLE_NAME}.encode("\\x75\\x74\\x66\\x2d\\x38")).decode("\\x75\\x74\\x66\\x2d\\x38"))'
-    return encryptcode(code)
+    content = encryptcode(content)
+    compiled_code = compile(content, "<string>", "exec")
+    bytecode = compiled_code.co_code
+    byte_values = list(bytecode)
+    obfuscated_code = f'exec(types.CodeType(0, 0, 0, 0, 0, b"{bytes(byte_values).decode("latin1")}", (), (), (), "", "", 0, b""))'
+    return obfuscated_code
 
 def main():
     parser = argparse.ArgumentParser(description='1line Obfuscator')
