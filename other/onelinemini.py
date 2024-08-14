fd4b8286d554a3d012b2fcf8f6401fe2407ccf16
43a1723 <hai1723notgay@gmail.com>
2024-08-14 09:27:40 +0700

    Update onelinemini.py

--- other/onelinemini.py
+++ other/onelinemini.py
@@ -2,38 +2,38 @@ import base64
 import marshal
 import bz2
 import argparse
+import types
 
-def encryptcode(codee):
-    compiled_code = compile(codee, '<string>', 'exec')
+def encryptcode(code):
+    compiled_code = compile(code, '<string>', 'exec')
     compressed_code = bz2.compress(marshal.dumps(compiled_code))
     compressed_code_str = repr(compressed_code)
-    return f"exec(__import__('marshal').loads(__import__('bz2').decompress({compressed_code_str}))"
+    return f"exec(__import__('marshal').loads(__import__('bz2').decompress({compressed_code_str})))"
 
 def obfuscate(content):
-    content = encryptcode(content)
-    compiled_code = compile(content, "<string>", "exec")
+    encrypted_content = encryptcode(content)
+    compiled_code = compile(encrypted_content, "<string>", "exec")
     bytecode = compiled_code.co_code
     byte_values = list(bytecode)
     obfuscated_code = f'exec(types.CodeType(0, 0, 0, 0, 0, b"{bytes(byte_values).decode("latin1")}", (), (), (), "", "", 0, b""))'
     return obfuscated_code
 
 def main():
-    parser = argparse.ArgumentParser(description='1line Obfuscator')
+    parser = argparse.ArgumentParser(description='1-line Obfuscator')
     parser.add_argument('-i', '--input', required=True, help='Input file')
     parser.add_argument('-o', '--output', required=True, help='Output file')
     parser.add_argument('-l', '--layer', type=int, default=1, help='Obfuscation layer (default: 1)')
 
     args = parser.parse_args()
 
-
-        
     print(f"Reading input file: {args.input}")
     with open(args.input, 'r', encoding='utf-8') as file:
         code = file.read()
-    for _ in range(args.layer):
+
+    for i in range(args.layer):
         code = obfuscate(code)
-        print(f"{_}\{args.layer} obfuscated\n{len(code)} bytes!")
-    
+        print(f"Layer {i + 1}/{args.layer} obfuscated\n{len(code)} bytes!")
+
     print(f"Writing obfuscated code to: {args.output}")
     with open(args.output, 'w', encoding='utf-8') as file:
         file.write(code)
