3ffca382f9330519d31a3ebabef059d03c64fdfa
43a1723 <hai1723notgay@gmail.com>
2024-08-31 16:25:21 +0700

    Update test.py

--- other/test.py
+++ other/test.py
@@ -8,7 +8,7 @@ def string_to_xor(byte_string):
     return f"bytes([b ^ {key} for b in {list(a)}][::-1])"
 
 def obfuscate(content):
-    a = string_to_xor(bz2.compress(content))
+    a = string_to_xor(bz2.compress(content.encode()))
     code = f"""
 def eexec():
     exec(bz2.decompress({a}))
