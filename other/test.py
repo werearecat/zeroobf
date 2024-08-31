90819192767c9bc3fc17a44309a815492102a12e
43a1723 <hai1723notgay@gmail.com>
2024-08-31 16:23:55 +0700

    Update test.py

--- other/test.py
+++ other/test.py
@@ -1,10 +1,18 @@
 import bz2
+import random
+
+def string_to_xor(byte_string):
+    key = random.randint(1, 255)
+    a = bytes([b ^ key for b in byte_string][::-1])
+    return f"bytes([b ^ {key} for b in {list(a)}][::-1])"
 
 def obfuscate(content):
-    code = """
+    a = string_to_xor(bz2.compress(content))
+    code = f"""
 def eexec():
-    exec("print('hi')")
+    exec(bz2.decompress({a}))
 
 exec(eexec.__code__)
 
     """
+    return code
