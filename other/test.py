0279c487314da13576742ceda9ae6baf88053559
43a1723 <hai1723notgay@gmail.com>
2024-08-31 16:24:37 +0700

    Update test.py

--- other/test.py
+++ other/test.py
@@ -1,5 +1,6 @@
 import bz2
 import random
+import argparse
 
 def string_to_xor(byte_string):
     key = random.randint(1, 255)
@@ -16,3 +17,24 @@ exec(eexec.__code__)
 
     """
     return code
+
+def main():
+    parser = argparse.ArgumentParser(description='XOR Obfuscator')
+    parser.add_argument('-i', '--input', required=True, help='Input file')
+    parser.add_argument('-o', '--output', required=True, help='Output file')
+    parser.add_argument('-l', '--layer', type=int, default=1, help='Obfuscation layer (default: 1)')
+    
+    args = parser.parse_args()
+
+    with open(args.input, 'r', encoding='utf-8') as file:
+        code = file.read()
+    
+    for _ in range(args.layer):
+        code = obfuscate(code)
+        print(f"{_}\\{args.layer} obfuscated\n{len(code)} bytes!")
+    
+    with open(args.output, 'w', encoding='utf-8') as file:
+        file.write(code)
+
+if __name__ == '__main__':
+    main()
